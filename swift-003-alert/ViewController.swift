e651555068a7739712e0b0c5262697ccbb6c4ff2
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-12 10:52:52 -0300

    :gift: ok

--- swift-003-alert/ViewController.swift
+++ swift-003-alert/ViewController.swift
@@ -3,11 +3,16 @@
 //  swift-003-alert
 //
 //  Created by Luiz Carlos da Silva Araujo on 12/08/21.
-//
+//  Following the tutorial:
+//  https://www.youtube.com/watch?v=v2uufD0MwPc&t=3s
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var btn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +20,32 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func mostrarAlert(_ sender: UIButton) {
+        
+        let alerta = UIAlertController(title: "Primeiro App", message: "Olá, mundo!", preferredStyle: UIAlertController.Style.alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alerta, animated: true, completion: nil)
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
